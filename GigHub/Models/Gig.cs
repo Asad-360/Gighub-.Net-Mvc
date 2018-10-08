f324b22468425c8e38f9ddb12103874166819b9e
Asad-Mukhtar <asadazad99@gmail.com>
2018-10-08 18:52:56 +0500

    Creating the gig model , second version

--- GigHub/Models/Gig.cs
+++ GigHub/Models/Gig.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,16 +9,18 @@ namespace GigHub.Models
     public class Gig
     {
         public int Id { get; set; }
-        
+
         // Who is making the gig
+        [Required]
         public ApplicationUser Artist { get; set; }
 
         //When the gig is made
         public DateTime DateTime { get; set; }
-
+        [Required]
+        [StringLength(250)]
         //Where the gig is hapenning
         public string Venue { get; set; }
-
+        [Required]
         //What genre it is
         public Genre Genre { get; set; }
 
