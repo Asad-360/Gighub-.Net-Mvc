d923d218b8859e1ba495155916298e6af6d061fa
Asad-Mukhtar <asadazad99@gmail.com>
2018-12-01 22:06:14 +0500

    changing home controller to the repository

--- GigHub/Controllers/HomeController.cs
+++ GigHub/Controllers/HomeController.cs
@@ -15,27 +15,25 @@ namespace GigHub.Controllers
     {
         private readonly ApplicationDbContext _context;
         private readonly AttendenceRepository _attendenceRepository;
+        private readonly GigRepository _gigRepository;
         public HomeController()
         {
             _context = new ApplicationDbContext();
             _attendenceRepository = new AttendenceRepository(_context);
+            _gigRepository = new GigRepository(_context);
         }
 
         public ActionResult Index(string query = null)
         {
-            var upcomingGigs = _context.Gigs
-                .Include(g => g.Artist)
-                .Include(g => g.Genre)
-                .Where(g =>
-                    g.DateTime > DateTime.Now
-                    && !g.IsCanceled);
+            var upcomingGigs = _gigRepository
+                .GetUpcomingGigs()
+                .Where(g=>!g.IsCanceled);
 
             if (!string.IsNullOrWhiteSpace(query))
             {
                 upcomingGigs = upcomingGigs.Where(g =>
                     g.Artist.Name.Contains(query) ||
                     g.Genre.Name.Contains(query) ||
-               
                     g.Venue.Contains(query));
             }
             string userId = User.Identity.GetUserId();
--- GigHub/Persistence/Repositories/GigRepository.cs
+++ GigHub/Persistence/Repositories/GigRepository.cs
@@ -10,6 +10,7 @@ namespace GigHub.Persistence.Repositories
     public class GigRepository : IGigRepository
     {
         private readonly ApplicationDbContext _context;
+
         public GigRepository(ApplicationDbContext context)
         {
             _context = context;
@@ -19,10 +20,10 @@ namespace GigHub.Persistence.Repositories
         {
             return _context.Gigs
                 .Include(g => Enumerable.Select<Attendence, ApplicationUser>(g.Attendences, a => a.Attendee))
-                .SingleOrDefault(g => g.Id ==id);
+                .SingleOrDefault(g => g.Id == id);
         }
 
-        public IEnumerable< Gig> GetGigsUserAttending(string userId)
+        public IEnumerable<Gig> GetGigsUserAttending(string userId)
         {
             return _context.Attendences
                 .Where(g => g.AttendeeId == userId)
@@ -43,13 +44,24 @@ namespace GigHub.Persistence.Repositories
                 .ToList();
         }
 
+        public IEnumerable<Gig> GetUpcomingGigs()
+        {
+            return  _context.Gigs
+                .Include(g => g.Artist)
+                .Include(g => g.Genre)
+                .Where(g =>
+                    g.DateTime > DateTime.Now
+                );
+        }
+
+
         public Gig GetGig(int gigId)
         {
             return _context
                 .Gigs
-                .Include(g=>g.Artist)
-                .Include(g=>g.Artist)
-                .SingleOrDefault( g => g.Id == gigId );
+                .Include(g => g.Artist)
+                .Include(g => g.Artist)
+                .SingleOrDefault(g => g.Id == gigId);
         }
 
         public void Add(Gig gig)
